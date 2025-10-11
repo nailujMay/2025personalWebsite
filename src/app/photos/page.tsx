380b94b5199fb61a5a7af576014f7004831833bf
Julian Yam <julianyam@Julians-MacBook-Pro.local>
2025-10-11 14:40:44 -0700

    fix photo path

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -3,34 +3,34 @@ import PageTitle from "../components/PageTitle";
 import Image from "next/image";
 
 const photoFiles = [
-    "../images/DSC00638.JPG",
-    "../images/DSC01222.png",
-    "../images/DSC01230.png",
-    "../images/DSC01319.JPG",
-    "../images/DSC01324.png",
-    "../images/DSC01420.png",
-    "../images/DSC01422.png",
-    "../images/DSCF2697.png",
-    "../images/DSCF2753.png",
-    "../images/DSCF2779.png",
-    "../images/DSCF2876.jpg",
-    "../images/DSCF2888.png",
-    "../images/DSCF2891.jpg",
-    "../images/DSCF2898.png",
-    "../images/DSCF2922.jpg",
-    "../images/DSCF2974 1.png",
-    "../images/DSCF3009.png",
-    "../images/DSCF3020.png",
-    "../images/DSCF3028.jpg",
-    "../images/DSCF3139.png",
-    "../images/DSCF3257.jpg",
-    "../images/DSCF3260.jpg",
-    "../images/DSCF3319.png",
-    "../images/DSCF3329.png",
-    "../images/DSCF3426.png",
-    "../images/DSCF3567.png",
-    "../images/DSCF4104.png",
-    "../images/DSCF4108.png"
+    "DSC00638.JPG",
+    "DSC01222.png",
+    "DSC01230.png",
+    "DSC01319.JPG",
+    "DSC01324.png",
+    "DSC01420.png",
+    "DSC01422.png",
+    "DSCF2697.png",
+    "DSCF2753.png",
+    "DSCF2779.png",
+    "DSCF2876.png",
+    "DSCF2888.png",
+    "DSCF2891.jpg",
+    "DSCF2898.png",
+    "DSCF2922.jpg",
+    "DSCF2974 1.png",
+    "DSCF3009.png",
+    "DSCF3020.png",
+    "DSCF3028.jpg",
+    "DSCF3139.png",
+    "DSCF3257.jpg",
+    "DSCF3260.jpg",
+    "DSCF3319.png",
+    "DSCF3329.png",
+    "DSCF3426.png",
+    "DSCF3567.png",
+    "DSCF4104.png",
+    "DSCF4108.png"
   ];
 
 
@@ -43,7 +43,7 @@ function Photos(){
       {photoFiles.map((file, i) => (
         <div key={i} className="overflow-hidden rounded-x ">
           <Image
-            src={`/photos/${file}`}   // Path inside /public
+            src={`/images/${file}`}   // Path inside /public
             alt={`Photo ${i}`}
             width={800}               // Give a default width
             height={600}              // Give a default height
