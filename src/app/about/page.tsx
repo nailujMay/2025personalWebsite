07ece2416e2c508c13096e95ae252d33615b6100
Julian Yam <julianyam@Julians-MacBook-Pro.local>
2025-10-25 23:20:08 -0700

    fix profile

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
       <Navbar />
       <div className="flex flex-col gap-[20px] ml-[60px] md:flex-row">
         <Image
-          src="/images/profile.jpg"
+          src={"/images/profile.JPG"}
           alt="About"
           width={800}
           height={600}
